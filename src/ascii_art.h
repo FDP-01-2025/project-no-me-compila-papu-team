c075bd0bedc5bb8824298a0677c945f28d5d6a99
apenate <edgar.aripenate@gmail.com>
2025-06-27 17:01:11 -0600

    Update in project structure to follow requirements

---
+++ src/ascii_art.h
@@ -0,0 +1,10 @@
+#ifndef ASCII_ART_H
+#define ASCII_ART_H
+
+// ASCII Art functions for game sprites
+void displayBattleStartSprite();
+void displayAngrySprite();
+void displayVictorySprite();
+void displayGameOverSprite();
+
+#endif 
