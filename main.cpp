c9f7426de3204e1422b5ab90205d389e0f80cafa
Ari Peñate <00021725@uca.edu.sv>
2025-07-07 20:42:30 -0600

    Fix in project for correct ejecution on windows

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include "src/SuperTriviaGame.h"
-#include "src/all_implementations.cpp"
 #include <cstdlib>
 #include <ctime>
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#undef byte
+#endif
+#include "src/SuperTriviaGame.h"
+#include "src/all_implementations.cpp"
 
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
     srand(time(0));
     SuperTriviaGame game;
     game.start();
